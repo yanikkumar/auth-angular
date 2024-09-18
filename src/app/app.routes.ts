736840a86639b68ddc6295d9599a0278a6c9f854
Yanik Kumar <yanikkumar@cdac.in>
2024-09-18 15:33:10 +0530

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginRegisterComponent } from './Pages/login-register/login-register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'loginregister',
+    pathMatch: 'full',
+  },
+  {
+    path: 'loginregister',
+    component: LoginRegisterComponent,
+  },
+];
