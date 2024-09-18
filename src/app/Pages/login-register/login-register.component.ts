0c1c3f8f7c0615922787613c5b931d77dc643e4d
Yanik Kumar <yanikkumar@cdac.in>
2024-09-18 16:27:51 +0530

    Register login toggle created

--- src/app/Pages/login-register/login-register.component.ts
+++ src/app/Pages/login-register/login-register.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login-register.component.html',
-  styleUrl: './login-register.component.css'
+  styleUrl: './login-register.component.css',
 })
 export class LoginRegisterComponent {
+  activeForm: 'login' | 'register' = 'register';
 
+  toggleForm(form: 'login' | 'register') {
+    this.activeForm = form;
+  }
 }
